763f728d93e1d533c30a644c9caedd34d771c0c7
Seun Agbeye <agbeyeseun1@gmail.com>
2019-03-28 03:52:08 +0100

    Revert "chore(modeling): database modeling:"

--- seeders/seed_stocks.py
+++
@@ -1,9 +0,0 @@
-"""Module for seeding users"""
-
-from api.models import Stock
-from .data import get_data
-
-
-def seed_stocks():
-    stock_data = get_data('stock')
-    Stock.bulk_create(stock_data)
